92ad017cefebffeb65fdd633b40dda774d6e343d
SmartWalle <smartwalle@gmail.com>
2018-07-23 13:03:28 +0800

    init

--- string.go
+++ string.go
@@ -6,14 +6,43 @@ import (
 )
 
 const (
-	k_SOURCE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	k_SOURCE_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	k_SOURCE_LOWER = "abcdefghijklmnopqrstuvwxyz"
+	k_SOURCE_NUM   = "0123456789"
 )
 
-func RandString(size int) (str string) {
+const (
+	K_RAND_TYPE_DEFAULT     = 0
+	K_RAND_TYPE_NUM_ONLY    = 1
+	K_RAND_TYPE_LOWER_ONLY  = 2
+	K_RAND_TYPE_UPPER_ONLY  = 3
+	K_RAND_TYPE_LOWER_NUM   = 4
+	K_RAND_TYPE_UPPER_NUM   = 5
+	K_RAND_TYPE_LOWER_UPPER = 6
+)
+
+var source = make(map[int]string)
+
+func init() {
+	source[K_RAND_TYPE_DEFAULT] = k_SOURCE_UPPER + k_SOURCE_LOWER + k_SOURCE_NUM
+	source[K_RAND_TYPE_NUM_ONLY] = k_SOURCE_NUM
+	source[K_RAND_TYPE_LOWER_ONLY] = k_SOURCE_LOWER
+	source[K_RAND_TYPE_UPPER_ONLY] = k_SOURCE_UPPER
+	source[K_RAND_TYPE_LOWER_NUM] =  k_SOURCE_LOWER + k_SOURCE_NUM
+	source[K_RAND_TYPE_UPPER_NUM] = k_SOURCE_UPPER + k_SOURCE_NUM
+	source[K_RAND_TYPE_LOWER_UPPER] = k_SOURCE_UPPER + k_SOURCE_LOWER
+}
+
+func RandString(size, rType int) (str string) {
+	var src = source[rType]
+	if src == "" {
+		src = source[K_RAND_TYPE_DEFAULT]
+	}
+
 	var r = rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < size; i++ {
-		idx := r.Intn(len(k_SOURCE) - 1)
-		str += k_SOURCE[idx : idx+1]
+		idx := r.Intn(len(src) - 1)
+		str += src[idx : idx+1]
 	}
 	return str
 }
--- string_test.go
+++ string_test.go
@@ -1,14 +1,16 @@
 package str4go
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestRandString(t *testing.T) {
-	fmt.Println(RandString(6))
-	fmt.Println(RandString(6))
-	fmt.Println(RandString(6))
-	fmt.Println(RandString(6))
-	fmt.Println(RandString(6))
+	fmt.Println(RandString(6, K_RAND_TYPE_DEFAULT))
+	fmt.Println(RandString(6, K_RAND_TYPE_NUM_ONLY))
+	fmt.Println(RandString(6, K_RAND_TYPE_LOWER_ONLY))
+	fmt.Println(RandString(6, K_RAND_TYPE_UPPER_ONLY))
+	fmt.Println(RandString(6, K_RAND_TYPE_LOWER_NUM))
+	fmt.Println(RandString(6, K_RAND_TYPE_UPPER_NUM))
+	fmt.Println(RandString(6, K_RAND_TYPE_LOWER_UPPER))
 }
